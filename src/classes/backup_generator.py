62c145d876f1352a6818ced64a796ee299ef65c9
eduardoemf <eduardomf.moura@gmail.com>
2024-06-05 21:38:05 -0300

    Update backup_generator.py

--- src/classes/backup_generator.py
+++ src/classes/backup_generator.py
@@ -1,21 +1,25 @@
 import os
 import shutil
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 
 class BackupManager:
     
-    def __init__(self, source_dir, backup_base_dir):
+    def __init__(self, source_dir, backup_base_dir, retention_days):
         """
-        Inicializa a classe com os diretórios de origem e destino do backup.
+        Inicializa a classe com os diretórios de origem e destino do backup, 
+        e o período de retenção dos backups.
 
         Args:
             source_dir (str): O caminho do diretório de origem.
             backup_base_dir (str): O caminho do diretório base onde o backup será armazenado.
+            retention_days (int): O número de dias para manter os backups.
         """
         self.source_dir = source_dir
         self.backup_base_dir = backup_base_dir
+        self.retention_days = retention_days
         self.setup_logging()
+
 
     def setup_logging(self):
         """
@@ -29,6 +33,7 @@ class BackupManager:
                 logging.StreamHandler()
             ]
         )
+
 
     def create_backup_directory(self, backup_dir_name):
         """
@@ -47,6 +52,7 @@ class BackupManager:
             logging.info(f"Criado diretório base de backup: {self.backup_base_dir}")
         
         return backup_dir
+
 
     def backup_directory(self):
         """
@@ -68,9 +74,44 @@ class BackupManager:
         except Exception as e:
             logging.error(f"Ocorreu um erro ao copiar o diretório: {e}")
 
+
+    def delete_old_backups(self):
+        """
+        Avalia os backups existentes e exclui aqueles que são mais antigos do que 
+        o número de dias de retenção especificado.
+
+        Returns:
+            bool: True se a exclusão foi realizada com sucesso, False caso contrário.
+        """
+        now = datetime.now()
+        cutoff_date = now - timedelta(days=self.retention_days)
+        backup_created_today = False
+
+        for folder_name in os.listdir(self.backup_base_dir):
+            folder_path = os.path.join(self.backup_base_dir, folder_name)
+            if os.path.isdir(folder_path):
+                try:
+                    folder_date_str = folder_name.split('_backup_')[-1]
+                    folder_date = datetime.strptime(folder_date_str, "%Y-%m-%d")
+                    if folder_date >= now.replace(hour=0, minute=0, second=0, microsecond=0):
+                        backup_created_today = True
+                    if folder_date < cutoff_date:
+                        shutil.rmtree(folder_path)
+                        logging.info(f"Backup antigo excluído: {folder_path}")
+                except Exception as e:
+                    logging.error(f"Erro ao avaliar ou excluir o diretório {folder_path}: {e}")
+                    return False
+        
+        if not backup_created_today:
+            logging.error("O backup de hoje não foi criado com sucesso. A exclusão de backups antigos foi abortada.")
+            return False
+        
+        return True
+
     def run_backup(self):
         """
         Executa todos os métodos necessários para realizar o backup em sequência.
         """
         self.setup_logging()
-        self.backup_directory()
+        if self.backup_directory():
+            self.delete_old_backups()
